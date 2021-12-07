e8d7f6a889b58b134eed3a8ac0aa05867d00d10f
Tomer Shimshi <tomershimshi@gmail.com>
2021-12-07 11:14:05 +0200

    added the first mutex call

--- EX3/main.c
+++ EX3/main.c
@@ -50,6 +50,7 @@ int main(int argc, char* argv[])
 	int number_of_real_pages = pow(2.0, physycal_bits);
 	Page_def* vir_pages = calloc(number_of_vir_pages, sizeof(Page_def));
 	Page_def* real_pages = calloc(number_of_real_pages, sizeof(Page_def));
+	DWORD wait_code;
 
 	if (vir_pages == NULL || real_pages == NULL) {
 		printf("Memory allocation to array of pages failed in main!");
@@ -74,5 +75,15 @@ int main(int argc, char* argv[])
 	if (Input_File_mutex_handle == NULL || Output_File_mutex_handle == NULL || vacent_pages_semaphore == NULL) {
 		printf("Memory allocation to mutex and semaphores failed in main!");
 		exit(1);
+	}
+
+	wait_code = WaitForSingleObject(Input_File_mutex_handle, INFINITE);
+	if (WAIT_OBJECT_0 != wait_code)
+	{
+
+		const int error = GetLastError();
+		printf("Error when waiting for mutex, error code: %d\n", error);
+		exit(1);
+
 	}
 }
